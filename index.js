c54de61cfa5e75f1425183032f9f2ff34eb9cccc
Natalie Barba <nbarba2010@gmail.com>
2021-08-11 15:02:38 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function superbowlWin(arrayOfObjects) {
+    function isWin(element) {
+        return (element["result"] === "W");
+    }
+    // return arrayOfObjects.find(isWin)["year"];
+    if (arrayOfObjects.find(isWin)) {
+        return arrayOfObjects.find(isWin)["year"];
+    } else {
+        return undefined;
+    }
+}
